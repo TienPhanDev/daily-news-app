5ee26ab3e7b2330afb7df8f675adcd3315b61420
tienphandev <tienphan.dev@gmail.com>
2021-07-01 01:24:41 -0400

    Upped

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+import * as React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid mx-auto px-4 mt-5">
+      <div className="mx-auto p-6 text-xl border border-solid border-blue-500">
+        My Daily News App
+      </div>
+      <div className="mx-auto p-4">
+        <Search onSearch={handleSearch} search={searchTerm}/>
+      </div>
+      <div >      
+        <List list={searchedStories} />
+      </div>
     </div>
   );
-}
+};
+
+const Search = (props) => (
+  <div className="p-4 mx-auto">
+    <label htmlFor="search">Search: </label>
+    <input id="search" value={props.search} type="text" onChange={props.onSearch} />
+  </div>
+)
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <div>
+      {props.item.author}
+      <a href={props.item.url}>{props.item.title}</a>
+    </div>
+    <div>{props.item.num_comments}</div>
+    <div>{props.item.points}</div>
+  </li>
+);
 
 export default App;
