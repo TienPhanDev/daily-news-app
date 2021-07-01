31349a0f92960a29f502871b561ec47189a6d7f8
tienphandev <tienphan.dev@gmail.com>
2021-07-01 02:12:59 -0400

    upped

--- src/App.js
+++ src/App.js
@@ -1,76 +1,79 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 /* eslint-disable func-names */
 /* eslint-disable prefer-arrow-callback */
-import * as React from "react";
-import "./App.css";
+import * as React from 'react';
+import './App.css';
 
 const App = () => {
   const stories = [
     {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
       objectID: 1,
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchTerm, setSearchTerm] = React.useState('');
 
-  const handleSearch = (event) => {
+  const handleSearch = event => {
     setSearchTerm(event.target.value);
   };
 
   const searchedStories = stories.filter(story =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
 
   return (
     <div className="grid mx-auto px-4 mt-5">
       <div className="mx-auto p-6 text-xl border border-solid border-blue-500">
         My Daily News App
+        {console.log(`${firstName} has a pet called ${name}`)}
       </div>
       <div className="mx-auto p-4">
-        <Search onSearch={handleSearch} search={searchTerm}/>
+        <Search onSearch={handleSearch} search={searchTerm} />
       </div>
-      <div >      
+      <div>
         <List list={searchedStories} />
       </div>
     </div>
   );
 };
 
-const Search = (props) => (
+const Search = ({ search, onSearch }) => {
   <div className="p-4 mx-auto">
     <label htmlFor="search">Search: </label>
-    <input id="search" value={props.search} type="text" onChange={props.onSearch} />
-  </div>
-)
+    <input id="search" type="text" value={search} onChange={onSearch} />
+  </div>;
+};
 
-const List = (props) => (
+const List = ({ list }) => (
   <ul>
-    {props.list.map((item) => (
-      <Item key={item.objectID} item={item} />
+    {list.map(item => (
+      <Item key={item.objectID} title={item.title} url={item.url} author={item.author} num_comments={item.num_comments} points={item.points} />
     ))}
   </ul>
 );
 
-const Item = (props) => (
+const Item = ({ title, url, author, num_comments, points }) => (
   <li>
     <div>
-      {props.item.author}
-      <a href={props.item.url}>{props.item.title}</a>
+      {author}
+      <a href={url}>{title}</a>
     </div>
-    <div>{props.item.num_comments}</div>
-    <div>{props.item.points}</div>
+    <div>{num_comments}</div>
+    <div>{points}</div>
   </li>
 );
 
