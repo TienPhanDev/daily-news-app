52f4d54164d1cf914221d591e7e588c01db459bd
tienphandev <tienphan.dev@gmail.com>
2021-07-02 00:23:22 -0400

    upped

--- src/App.js
+++ src/App.js
@@ -4,81 +4,173 @@
 import * as React from 'react';
 import './App.css';
 
-const useCustomHook = () => {
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem('search') || '',
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
+const useCustomHook = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState,
   );
 
   React.useEffect(() => {
-    localStorage.setItem('search', searchTerm);
-  }, [searchTerm]);
+    localStorage.setItem(key, value);
+  }, [value, key]);
 
-  return [searchTerm, setSearchTerm]
+  return [value, setValue];
+};
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.filter(story => action.payload.objectID !== story.objectID),
+      };
+    default:
+      throw new Error();
+  }
 };
 
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+  const [searchTerm, setSearchTerm] = useCustomHook('search', 'React');
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
 
-  const [searchTerm, setSearchTerm] = useCustomHook();
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+  };
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.filter(story =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
+  React.useEffect(() => {
+    if (!searchTerm) return;
+
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+    fetch(url)
+      .then(response => response.json())
+      .then(result => {
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.hits,
+        });
+      })
+      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+  }, [searchTerm, url]);
+
+  const handleRemove = item => {
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
+  };
 
   return (
-    <div className="grid mx-auto px-4 mt-5">
-      <div className="mx-auto p-6 text-xl border border-solid border-blue-500">
-        My Daily News App
+    <div className="grid px-4 mt-5">
+      <div className="mx-auto p-4 text-lg border border-solid border-blue-500">
+        Daily News
       </div>
       <div className="mx-auto p-4">
-        <Search onSearch={handleSearch} search={searchTerm} />
+        <CustomInput
+          id="search"
+          value={searchTerm}
+          isFocused
+          onInputChange={handleSearch}
+        >
+          <strong>Search:</strong>
+        </CustomInput>
       </div>
+      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
+        Submit
+      </button>
       <div>
-        <List list={searchedStories} />
+        {stories.isError && <p>Something went wrong ...</p>}
+        {stories.isLoading ? (
+          <p>Loading.....</p>
+        ) : (
+          <List list={stories.data} onRemoveItem={handleRemove} />
+        )}
       </div>
     </div>
   );
 };
 
-const Search = ({ search, onSearch }) => (
-  <div className="p-4 mx-auto">
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" value={search} onChange={onSearch} />
-  </div>
-);
+const CustomInput = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  const inputRef = React.useRef();
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
 
-const List = ({ list }) =>
-  list.map(item => <Item key={item.objectID} {...item} />);
+const List = ({ list, onRemoveItem }) =>
+  list.map(item => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
 
-const Item = ({ title, url, author, numComments, points }) => (
+const Item = ({ item, onRemoveItem }) => (
   <div>
     <div>
-      {author}
-      <a href={url}>{title}</a>
+      {item.author}
+      <a href={item.url}>{item.title}</a>
     </div>
-    <div>{numComments}</div>
-    <div>{points}</div>
+    <div>{item.numComments}</div>
+    <div>{item.points}</div>
+    <span>
+      <button
+        type="button"
+        onClick={() => onRemoveItem(item)}
+        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+      >
+        Delete
+      </button>
+    </span>
   </div>
 );
 
